736af2c4d6f5a88f6e03cf7bbea219fefbc7f70d
mrdrebot <m.r.drebot.j@gmail.com>
2021-01-25 00:37:59 +0200

    did home work 2021-01-25_Node-http-server, request and responce

--- index.js
+++ index.js
@@ -1,95 +1,117 @@
-/* eslint-disable indent */
-/* eslint-disable no-tabs */
 /*
-Все операции с файлами производить с помощью билиотеки fs и асинхронных функций:
-1) создаём 2 папки, в одну папку кладём картинку, например SVG-файл. Эту картинку перемещаем из одной папки в другую;
-2) из прошлой домашки делаем request на dou.ua, добываем массив адресов картинок, сохраняем его в файл arr.txt;
-3) запускаем сервер на встроенном https, выводим картинки из файла arr.txt.
+  При заходе на страницу 127.0.0.1 должна отобразиться таблица (Бренд ,Модель, Год выпуска, Цена) с автомобилями, спарсенными с сайта auto.ria.ua.
 */
 
-//  Подключенные модули
-const fs = require('fs');
+//  Подключение модулей
 const request = require('request');
+const http = require('http');
 
-//	Задание №1
-//  Начало кода
-const firstFolderName = 'folderIncFile';
-const secondFolderName = 'folderMoveFile';
-const startPath = './dragon.jpg';
-const oldPath = `./${firstFolderName}/dragon.jpg`;
-const newPath = `./${secondFolderName}/dragon.jpg`;
+const carArr = [];
 
-//  Создание папки содержащей файл
-fs.mkdir(`./${firstFolderName}`, (createFolderErr) => {
-	if (createFolderErr) throw createFolderErr;
-});
+//  Функция создания массива строк
+const findStr = (dataStr, str1, str2, position) => {
+  const tempArr = [];
 
-// Копирование файл в парку
-fs.copyFile(startPath, oldPath, (err) => {
-  if (err) {
-    throw err;
-  } else {
-		console.log(`Successfully copied file "${startPath.slice(2)}"`);
+  while (true) {
+    const starPos = dataStr.indexOf(str1, position);
+    const endPos = dataStr.indexOf(str2, starPos);
+    const foundData = dataStr.slice(starPos, endPos);
 
-    // Создание папки в которую будет перемещен файл
-		fs.mkdir(`./${secondFolderName}`, (createFolderErr) => {
-			if (createFolderErr)	{
-				throw createFolderErr;
-			}
-		});
+    if (starPos === -1) break; //  Выход если в полученой строке, нет искомого начала текста
 
-		// Перемещение файла
-		fs.rename(oldPath, newPath, (errMove) => {
-			if (errMove) throw err;
-			console.log(`Successfully moved file "${startPath.slice(2)}"`);
-		});
+    tempArr.push(foundData);
+
+    position = endPos; // Начальная позиция старта для следующего объекта
   }
-});
 
-//	Задание №2
-const fileName = 'arr.txt';
-//  Запуск модуля обращения к странице
-request('https://dou.ua/', (error, response, body) => {
-    if (error) {
-        console.error('error:', error);
+  return tempArr;
+};
+
+//  Функция получения значения из строки
+const getVal = (tempArr, str1, str2, val) => {
+  tempArr.filter((el) => el.indexOf(str1, 0) > -1).forEach((el, i) => {
+    const starPos = el.indexOf(str1, 0);
+    const endPos = el.indexOf(str2, starPos);
+
+    if (typeof carArr[i] !== 'object') {
+      if (typeof str2 !== 'string') {
+        carArr.push({
+          [val]: el.slice(starPos).slice(str1.length),
+        });
+      } else {
+        carArr.push({
+          [val]: el.slice(starPos, endPos).slice(str1.length),
+        });
+      }
     } else {
-        let pos = 0; //  Начальная позиция старта поска строки
-        const imgTagArr = [];
-        const strFindValBeg = '<img'; //  Начало искомого текста
-				const strFindValEnd = '>'; //  Конец искомого текста
-
-        //  Цикл перебора полученной строки с сайта
-        while (true) {
-            const starPos = body.indexOf(strFindValBeg, pos);
-            const endPos = body.indexOf(strFindValEnd, starPos);
-            const foundImgTag = body.slice(starPos, endPos);
-
-            if (starPos === -1) break; //  Выход если в полученой строке, нет искомого начала текста
-
-            imgTagArr.push(foundImgTag);
-
-            pos = endPos; // Начальная позиция старта для следующего объекта
-				}
-
-				fs.open(fileName, 'w', (errCreateFile) => {
-					if (errCreateFile) throw errCreateFile;
-					console.log(`File ${fileName} created!`);
-    		});
-
-				// console.log(imgTagArr);
-
-        //  Цикл получения имени файла, url файла и записи файла на диск
-        imgTagArr.forEach((el) => {
-            const pos2 = 0;
-            const starPos = el.indexOf('src=', pos2);
-						const endPos = el.indexOf(' ', starPos);
-
-            el = el.slice(starPos, endPos).slice(5, -1); //  Удаление с начала и конца ""
-
-						fs.appendFile(fileName, `${el}\n`, (errWriteInFile) => {
-							if (errWriteInFile) throw errWriteInFile;
-							console.log('Data has been added!');
-			      });
-				});
+      const temp = carArr[i];
+      if (typeof str2 !== 'string') {
+        temp[val] = el.slice(starPos).slice(str1.length);
+      } else {
+        temp[val] = el.slice(starPos, endPos).slice(str1.length);
+      }
     }
+  });
+};
+
+//  Запуск модуля обращения к странице
+request('https://auto.ria.com/uk/search/?indexName=auto,order_auto,newauto_search&body.id[4]=2&year[0].gte=2012&year[0].lte=2012&categories.main.id=1&brand.id[0]=6&price.currency=1&gearbox.id[1]=2&gearbox.id[2]=3&fuel.id[1]=2&drive.id[0]=1&abroad.not=0&custom.not=1&page=0&size=50', (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    //  Создание массива объектов продаваемых авто
+    let dataStrArr = [];
+    const strFindValBeg = '<div class="hide"'; //  Начало искомого текста
+    const strFindValEnd = '>'; //  Конец искомого текста
+
+    const strFindValBeg2 = 'main-price'; //  Начало искомого текста
+    const strFindValEnd2 = '>'; //  Конец искомого текста
+
+    dataStrArr = findStr(body, strFindValBeg, strFindValEnd, 0);
+
+    getVal(dataStrArr, 'mark-name="', '" ', 'name');
+    getVal(dataStrArr, 'model-name="', '" ', 'model');
+    getVal(dataStrArr, 'data-year="', '" ', 'year');
+
+    dataStrArr = findStr(body, strFindValBeg2, strFindValEnd2, 0);
+
+    getVal(dataStrArr, 'main-price="', '"\n', 'price');
+
+    //  Создаем таблицу
+    let table = '';
+
+    //  Получение значений для шапки таблицы
+    const objVal = Object.getOwnPropertyNames(carArr[0]);
+    // console.log(objVal);
+
+    //  Создание шапки талицы
+    let tempStr = '';
+
+    objVal.forEach((el) => {
+      tempStr += `<th>${el}</th>`;
+    });
+
+    const tableFirstRow = `<tr>${tempStr}</tr>`;
+
+    //  Создание тела таблицы
+    let tableBody = '';
+
+    carArr.forEach((el) => {
+      tableBody += `<tr><td>${el.name}</td><td>${el.model}</td><td>${el.year}</td><td>${el.price}</td></tr>`;
+    });
+
+    //  Создание полноценной таблицы
+    table = `<table border="1">${tableFirstRow}${tableBody}</table>`;
+
+    //  Создаем http-server
+    const server = http.createServer((req, res) => {
+      res.write(table);
+      res.end();
+    });
+
+    //  Прошлушивание обращений на порт 3000
+    server.listen(3000, () => {
+      console.log('server port opened: ', server.address().port);
+    });
+  }
 });
