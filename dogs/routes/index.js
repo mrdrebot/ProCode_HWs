a7596d817891cca29cbbb49028644625d88e6801
mrdrebot <m.r.drebot.j@gmail.com>
2021-02-06 23:34:13 +0200

    did home work 2021-02-08_Node_promise-dogs

--- dogs/routes/index.js
+++ dogs/routes/index.js
@@ -1,16 +1,76 @@
-const express = require('express');
-const request =  require('request');
-const router = express.Router();
+var express = require('express');
+var axios = require('axios');
+var router = express.Router();
 
-/* Refresh page */
+let url = 'https://dog.ceo/api/breeds/list/all';
+
+
+// Задание 3
+// Создание страницы при отсутствии данных в строке адреса браузера
 router.get('/', function(req, res, next) {
-   
-  request('https://dog.ceo/api/breeds/image/random', (err, resp, body) => {
-    const pictDogUrl = JSON.parse(body);
-    pictDogUrl.title = 'Choose your dog!';
-    pictDogUrl.phrase = 'See and choose';
-    res.render('index', pictDogUrl);
-  });
+  //  Формирование запроса на адрес по умолчанию  
+  axios.get(url)
+      .then(r1 => r1.data.message)  //  получения объкта со всеми породами
+      .then(r2 => {                 //  создание объекта передаваемого в index.ejs
+        let renderObj = {};
+        renderObj.title = 'Dog breeds';
+        renderObj.breed = '';       //  Если выбора породы небыло, не отображать тэг <img>
+        renderObj.breeds = r2;
+        res.render('index', renderObj);
+      })
+      .catch(err => console.log(err));
 });
+
+// Создание страницы при наличии данных в строке адреса браузера (выбрана порода)
+router.get('/:breed', function(req, res, next) {
+  axios.get(url)
+    .then(r1 => r1.data.message)
+    .then(r2 => {                   // Формирование запроса на фото выбраной породы
+      axios.get(`https://dog.ceo/api/breed/${req.params.breed}/images/random`)
+      .then(r21 => {                //  создание объекта передаваемого в index.ejs
+        let renderObj = {};
+        renderObj.title = 'Dog breeds';
+        renderObj.breed = req.params.breed;
+        renderObj.breeds = r2;
+        renderObj.url = r21.data.message;
+        res.render('index', renderObj);
+      })
+      .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
+
+// Задание 2
+
+// router.get('/', function(req, res, next) {
+//   axios.get(url)
+//     .then(r1 => r1.data.message)
+//     .then(r2 => {
+//       let renderObj = {};
+//       renderObj.title = 'Dog breeds';
+//       renderObj.breeds = r2;
+//       res.render('index', renderObj);
+//     })
+//     .catch(err => console.log(err));
+// });
+
+// Задание 1
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   const promise2000 = () => {
+//     let back = new Promise((resolve, reject) => {
+//       setTimeout(() => {
+//         resolve();
+//       }, 2000);
+//     });
+
+//     return back;
+//   }
+
+//   promise2000()
+//   .than(res.render('index', { title1: 'Express' }))
+//   .catch(err => console.log(err));
+// });
 
 module.exports = router;
