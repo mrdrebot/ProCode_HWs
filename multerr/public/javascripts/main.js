563b802264f35e81a20d8ae87d4ea4caf9126c97
mrdrebot <m.r.drebot.j@gmail.com>
2021-02-28 14:17:17 +0200

    did home work 2021-03-01_hw_Node_multer-csv-regex

--- multerr/public/javascripts/main.js
+++ multerr/public/javascripts/main.js
@@ -1,10 +1,63 @@
-const form = document.querySelector("form");
-const answEl = document.querySelector(".answ");
+// Функции
+const checkPatern = (field, patern) => {
+  if(field.value.match(patern) === null) {
+    field.classList.add("error");
+    return 0;
+  }
+  else {
+    field.classList.remove("error");
+    return 1;
+  }
+}
 
+// Переменные
+const form = document.querySelector("form");
+const fieldsArr = [];
+const answEl = document.querySelector(".answ");
+const authorNameEl = document.querySelector("#authorName");
+const dateEl = document.querySelector("#date");
+const articleEl = document.querySelector("#article");
+
+// Наполнение массива полями вводимых данных, которые необходимо проверить
+for(i = 0; i < form.length; i++) {
+  if(form[i].tagName !== "BUTTON") {
+    fieldsArr.push(form[i]);
+  }
+}
+
+// Создание обработчика события отправки на сервер данных
 form.addEventListener("submit", function(event) {
-    event.preventDefault();
-    const data = new FormData(form);
-    axios.post('/', data)
-      .then(r => answEl.innerHTML = r.data)
-      .catch(e => answEl.innerHTML = `ERROR: ${e}`);
+  event.preventDefault();                               // отключение действия по умолчанию у кнопки
+  // Шаблоны проверки
+  const namePatern = /^[a-zа-я-\.]+\s[a-zа-я-\.]+$/gi;  
+  const datePatern = /^\d{2}-\d{2}-\d{4}$/;
+  const articlePatern = /^[a-z0-9\s\.,]+$/gi;
+  let checkField = 0;                                   // Флаг прерывания отправки данных на сервер
+
+  // Цикл обработки всех полей введенных данных
+  fieldsArr.forEach(el => {
+      switch (el.name) {
+        case "authorName":
+          if (checkPatern(authorNameEl, namePatern) === 0) checkField++;
+          break;
+        case "date":
+          if (checkPatern(dateEl, datePatern) === 0) checkField++;
+          break;
+        case "article":
+          if (checkPatern(articleEl, articlePatern) === 0) checkField++;
+      }
+    });
+  
+  // Если значение флага больше нуля, прерывается отправка данных на сервер
+  if(checkField > 0) return;
+
+  const data = new FormData(form);
+  axios.post('/', data)
+    .then(r => {
+      answEl.innerHTML = r.data;
+      authorNameEl.value = '';
+      dateEl.value = '';
+      articleEl.value = '';
+    })
+    .catch(e => answEl.innerHTML = `Server respons error (POST):\n ${e}`);
 });
--- multerr/routes/index.js
+++ multerr/routes/index.js
@@ -1,44 +1,60 @@
 const express = require('express');
 const router = express.Router();
-const moment = require('moment');
+const multer = require('multer');
+const upload = multer();
 const fs = require('fs');
 const fsProm = require('fs/promises');
-const multer = require('multer');
+const moment = require('moment');
+const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    const date = moment().format('YYYY-MM-DD');
-    const filePath = `./public/images/${date}`;
-    fs.stat(filePath, (err) => {
-      if (err === null) {
-        cb(null, filePath);
-      }
-      else if (err.code === 'ENOENT') {
-        fsProm.mkdir(filePath)
-        .then(cb(null, filePath))
-        .catch(err => console.log(err));
-      }
-      else {
-        console.log('Something go wrong!\n', err);
-      }
-    });
-  },
-  filename: function (req, file, cb) {
-    const fileNameObj = req.body;
-    const fileExtension = file.originalname.slice(file.originalname.lastIndexOf('.'));
-    cb(null, `${fileNameObj.fileName}${fileExtension}`)
-  }
-});
+// Функция создания файла
+async function createThenClose(path, name) { 
+  let filehandle = null; 
 
-const upload = multer({ storage: storage });
+  try { 
+      // Using the filehandle method  
+      filehandle = await fsProm.open(`${path}/${name}.csv`, 'w'); 
+      filehandle.close(); 
+  } catch (error) { 
+      console.log("Create file error:", error); 
+  } 
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Download your file' });
+  res.render('index', { title: 'Create your csv file' });
 });
 
-router.post('/', upload.single('file'), function(req, res, next) {
-  res.send('File loaded and processed!');
+// Получение данных с формы, создание файла и отправка результата 
+router.post('/', upload.none(), function(req, res, next) {
+  const date = moment().format('YYYY-MM-DD');
+  const filePath = `./public/csv/${date}`;
+  const dataStr = Object.values(req.body).join(';') + '\n'; // преобразование данных с формы в строку
+  
+  // Проверка наличия папки для сохранения файлов
+  fs.stat(filePath, (err) => {
+    if (err === null) {
+      fsProm.appendFile(`${filePath}/${date}.csv`, dataStr)
+      .catch(err => console.log('Add data in the file: ', err));
+    }
+    else if (err.code === 'ENOENT') {
+      fsProm.mkdir(filePath)
+      .then(createThenClose(filePath, date))
+      .then(fsProm.appendFile(`${filePath}/${date}.csv`, dataStr))
+      .catch(err => console.log('Create folder error and add data in the file', err));
+    }
+    else {
+      console.log('Check path to create folder error:\n', err);
+    }
+  });
+  
+  res.send(`Data downloaded and saved in a file csv!<br><a href="${filePath}/${date}.csv" download>Download file</a>`);
+});
+
+// Отправка пути для скачивния файла
+router.get(`/:folder1/:folder2/:folder3/:file.csv`, function(req, res, next) {
+  const filePath = `../${req.params.folder1}/${req.params.folder2}/${req.params.folder3}/${req.params.file}.csv`;
+  res.sendFile(path.join(__dirname, filePath));
 });
 
 module.exports = router;
